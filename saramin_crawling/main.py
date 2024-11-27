b195ec8f44eb44733d331f92a7b58af026b9dec3
Yoo-SH <ads137485@jbnu.ac.kr>
2024-11-28 03:46:33 +0900

    feat: postgresql 연결 및 환경변수 설정

--- saramin_crawling/main.py
+++ saramin_crawling/main.py
@@ -1,6 +1,12 @@
 from params_code import *
+from dotenv import load_dotenv
+import os
 from crawler import crawl_saramin
+from sqlalchemy import create_engine
 
+
+# .env 파일에서 환경 변수 로드
+load_dotenv()
 
 # 사용 예시
 if __name__ == "__main__":
@@ -11,3 +17,18 @@ if __name__ == "__main__":
 
     df = crawl_saramin(keyword, pages)
     print(df)
+
+    # postgresql 연결
+    engine = create_engine(
+        f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}",
+        connect_args={"client_encoding": "utf8"},
+    )
+
+    try:
+        # 연결 테스트
+        with engine.connect() as connection:
+            print("연결 성공")
+            # PostgreSQL 데이터베이스에 연결하여 크롤링한 데이터를 "jobs" 테이블에 저장합니다.
+            df.to_sql("jobs", engine, if_exists="append", index=False)
+    except Exception as e:
+        print("연결 실패:", e)
