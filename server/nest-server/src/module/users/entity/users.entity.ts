1ab745fa566d0d3b5d523c814affd8e4f793929f
Yoo-SH <ads137485@jbnu.ac.kr>
2024-11-29 02:09:05 +0900

    feat: user 엔터티 추가

---
+++ server/nest-server/src/module/users/entity/users.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt: Date;
+
+    @DeleteDateColumn()
+    deleteAt: Date;
+
+}
