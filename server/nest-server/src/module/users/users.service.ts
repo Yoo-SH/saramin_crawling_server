ece0a46f4a5d92c687b1a02395325ca3ed67bf8a
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-01 03:52:16 +0900

    feat: jwtStrategy validation을 위해 일부 함수 추가

--- server/nest-server/src/module/users/users.service.ts
+++ server/nest-server/src/module/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { Users } from './entity/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(Users) private repo_user: Repository<Users>) { }
+
+    /*사용자 ID로 사용자 조회*/
+    async findById(user_id: number) {
+        const user = await this.repo_user.findOne({ where: { id: user_id } });
+
+        if (!user) {
+            throw new NotFoundException(`해당 id의 사용자가 존재하지 않습니다.`);
+        }
+
+        return user;
+    }
+}
