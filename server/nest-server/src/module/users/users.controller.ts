bd6395ffb3e85400e91a24da8731629dd873f695
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-01 20:54:44 +0900

    feat: 사용자 정보 조회 API 추가, all, name, id

--- server/nest-server/src/module/users/users.controller.ts
+++ server/nest-server/src/module/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private UsersService: UsersService) { }
+
+    @Get('all')
+    findAllUsers() {
+        return this.UsersService.findAllUsers();
+    }
+
+
+    @Get('search')
+    findUsersByName(@Query('name') name?: string) {
+        return this.UsersService.findUsersByName(name);
+    }
+
+    @Get(':id')
+    findUsersById(@Param('id') id: number) {
+        return this.UsersService.findUsersById(id);
+    }
+
+}
+
--- server/nest-server/src/module/users/users.service.ts
+++ server/nest-server/src/module/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpCode, HttpStatus, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
@@ -8,14 +8,76 @@ import { Users } from './entity/users.entity';
 export class UsersService {
     constructor(@InjectRepository(Users) private repo_user: Repository<Users>) { }
 
-    /*사용자 ID로 사용자 조회*/
-    async findById(user_id: number) {
-        const user = await this.repo_user.findOne({ where: { id: user_id } });
+    /*모든 사용자 조회*/
+    async findAllUsers() {
+        try {
+            const users = await this.repo_user.find();
 
-        if (!user) {
-            throw new NotFoundException(`해당 id의 사용자가 존재하지 않습니다.`);
+            if (!users) {
+                throw new NotFoundException(`사용자가 존재하지 않습니다.`);
+            }
+
+            return {
+                messege: "사용자 조회 성공",
+                data: users,
+                statusCode: HttpStatus.OK
+            }
+
+        } catch (error) {
+            throw new InternalServerErrorException(`사용자 조회 중 오류가 발생했습니다.`);
+        }
+    }
+
+    /*사용자 ID로 사용자 조회*/
+    async findUsersById(user_id: number) {
+        try {
+            const user = await this.repo_user.findOne({ where: { id: user_id } });
+
+            if (!user) {
+                throw new NotFoundException(`해당 id의 사용자가 존재하지 않습니다.`);
+            }
+
+            return {
+                messege: "사용자 조회 성공",
+                data: {
+                    user_id: user.id,
+                    user_name: user.name
+                },
+                statusCode: HttpStatus.OK
+            }
+
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new InternalServerErrorException(`사용자 조회 중 오류가 발생했습니다.`);
         }
 
-        return user;
+    }
+
+    /*사용자 이름으로 사용자 조회*/
+    async findUsersByName(user_name: string) {
+        try {
+            const user = await this.repo_user.findOne({ where: { name: user_name } });
+
+            if (!user) {
+                throw new NotFoundException(`해당 이름의 사용자가 존재하지 않습니다.`);
+            }
+
+            return {
+                messege: "사용자 조회 성공",
+                data: {
+                    user_id: user.id,
+                    user_name: user.name
+                },
+                statusCode: HttpStatus.OK
+            }
+
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new InternalServerErrorException(`사용자 조회 중 오류가 발생했습니다.`);
+        }
     }
 }
