16f2642f86ff2615771110da735eb4b14695cef2
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-06 03:52:13 +0900

    feat: 성능을 모니터링하는 로깅 시스템 추가

---
+++ server/nest-server/src/common/interceptor/performance-logging.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+    Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class PerformanceLoggingInterceptor implements NestInterceptor {
+    private readonly logger = new Logger(PerformanceLoggingInterceptor.name);
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        // 요청 정보를 가져옵니다.
+        const req = context.switchToHttp().getRequest();
+        const { method, url } = req;
+        const startTime = Date.now(); // 요청 시작 시간 기록
+
+        this.logger.log(`Incoming Request: [${method}] ${url}`);
+
+        return next.handle().pipe(
+            tap(() => {
+                const endTime = Date.now(); // 응답이 끝난 시간 기록
+                const responseTime = endTime - startTime; // 총 소요 시간 계산
+
+                this.logger.log(
+                    `Outgoing Response: [${method}] ${url} - Response Time: ${responseTime}ms`,
+                );
+            }),
+        );
+    }
+}
