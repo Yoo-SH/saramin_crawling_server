ec95ffa5657c69d8178b67d0c35ec20ddd0462cc
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-07 18:40:32 +0900

    feat: jobs API 에러 코드 추가

--- server/nest-server/src/module/jobs/jobs.controller.ts
+++ server/nest-server/src/module/jobs/jobs.controller.ts
@@ -11,6 +11,7 @@ import { ResponseGetJobsIdDto } from './dto/response/response-get-jobs-id.dto';
 import { ResponsePostJobsDto } from './dto/response/response-post-jobs.dto';
 import { ResponsePutJobsIdDto } from './dto/response/response-put-jobs-id.dto';
 import { ResponseDeleteJobsIdDto } from './dto/response/response-delete-jobs-id.dto';
+import { ErrorResponseDto } from 'src/common/dto/error-response.dto';
 
 @ApiTags('jobs')
 @Controller('jobs')
@@ -19,6 +20,7 @@ export class JobsController {
 
     @ApiOperation({ summary: '모든 채용공고 조회' })
     @ApiResponse({ status: 200, description: '성공', type: ResponseGetJobsDto })
+    @ApiResponse({ status: 400, description: '실패', type: ErrorResponseDto })
     @Get()
     async getJobs(@Query() getJobsDto: GetJobsDto) {
         return await this.jobsService.getJobs(getJobsDto);
@@ -26,6 +28,8 @@ export class JobsController {
 
     @ApiOperation({ summary: '특정 채용공고 조회' })
     @ApiResponse({ status: 200, description: '성공', type: ResponseGetJobsIdDto })
+    @ApiResponse({ status: 404, description: '해당 공고가 없습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '공고 상세 조회 중 서버에서 에러가 발생했습니다.', type: ErrorResponseDto })
     @Get(':id')
     async getJob(@Param('id') id: number) {
         return await this.jobsService.getJob(id);
@@ -33,6 +37,7 @@ export class JobsController {
 
     @ApiOperation({ summary: '채용공고 생성' })
     @ApiResponse({ status: 201, description: '성공(data는 body와 동일)', type: ResponsePostJobsDto })
+    @ApiResponse({ status: 500, description: '공고 등록 중 서버에서 에러가 발생했습니다.', type: ErrorResponseDto })
     @UseGuards(JwtAuthGuard)
     @Post()
     async createJob(@Body() createJobsDto: CreateJobsDto) {
@@ -41,6 +46,8 @@ export class JobsController {
 
     @ApiOperation({ summary: '채용공고 수정' })
     @ApiResponse({ status: 201, description: '성공(data는 body와 동일)', type: ResponsePutJobsIdDto })
+    @ApiResponse({ status: 404, description: '해당 공고가 없습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '공고 수정 중 서버에서 에러가 발생했습니다.', type: ErrorResponseDto })
     @UseGuards(JwtAuthGuard)
     @Put(':id')
     async updateJob(@Param('id') id: number, @Body() UpdateJobsDto: UpdateJobsDto) {
@@ -49,6 +56,8 @@ export class JobsController {
 
     @ApiOperation({ summary: '채용공고 삭제' })
     @ApiResponse({ status: 201, description: '성공(data는 body와 동일)', type: ResponseDeleteJobsIdDto })
+    @ApiResponse({ status: 404, description: '해당 공고가 없습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '공고 삭제 중 서버에서 에러가 발생했습니다.', type: ErrorResponseDto })
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
     async deleteJob(@Param('id') id: number) {
