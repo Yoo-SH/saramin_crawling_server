c82643b0ec58c2bcbe3f0b5f4f055b6077b9f745
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-07 18:55:48 +0900

    feat: user API 에러 코드 추가

--- server/nest-server/src/module/users/users.controller.ts
+++ server/nest-server/src/module/users/users.controller.ts
@@ -4,6 +4,7 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ResponseGetUsersAllDto } from './dto/response/response-get-users-all.dto';
 import { ResponseGetUsersSearchDto } from './dto/response/response-get-users-search.dto';
 import { ResponseGetUsersIdDto } from './dto/response/response-get-users-id.dto';
+import { ErrorResponseDto } from 'src/common/dto/error-response.dto';
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
@@ -11,6 +12,8 @@ export class UsersController {
 
     @ApiOperation({ summary: '모든 유저 조회' })
     @ApiResponse({ status: 200, description: '성공(data는 array)', type: ResponseGetUsersAllDto })
+    @ApiResponse({ status: 404, description: '사용자가 존재하지 않습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '사용자 조회 중 오류가 발생했습니다.', type: ErrorResponseDto })
     @Get('all')
     findAllUsers() {
         return this.UsersService.findAllUsers();
@@ -19,6 +22,8 @@ export class UsersController {
 
     @ApiOperation({ summary: '유저 이름으로 특정 유저 조회' })
     @ApiResponse({ status: 200, description: '성공', type: ResponseGetUsersSearchDto })
+    @ApiResponse({ status: 404, description: '해당 이름의 사용자가 존재하지 않습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '사용자 조회 중 오류가 발생했습니다.', type: ErrorResponseDto })
     @Get('search')
     findUsersByName(@Query('name') name?: string) {
         return this.UsersService.findUsersByName(name);
@@ -26,6 +31,8 @@ export class UsersController {
 
     @ApiOperation({ summary: '특정 유저 조회' })
     @ApiResponse({ status: 200, description: '성공', type: ResponseGetUsersIdDto })
+    @ApiResponse({ status: 404, description: '해당 id의 사용자가 존재하지 않습니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 500, description: '사용자 조회 중 오류가 발생했습니다.', type: ErrorResponseDto })
     @Get(':id')
     findUsersById(@Param('id') id: number) {
         return this.UsersService.findUsersById(id);
