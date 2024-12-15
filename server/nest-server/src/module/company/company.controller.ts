d27869a254538b83966f3b456080c80e73dd8e94
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-15 21:29:04 +0900

    feat: company validation res api 추가

--- server/nest-server/src/module/company/company.controller.ts
+++ server/nest-server/src/module/company/company.controller.ts
@@ -12,6 +12,10 @@ export class CompanyController {
 
     @Post()
     @ApiResponse({ status: 201, description: '회사 생성 성공', type: ResponsePostCompanyDto })
+    @ApiResponse({ status: 400, description: '회사 ID는 숫자여야 합니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 400, description: '회사 ID는 필수 입력 값입니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 400, description: '회사명은 문자열이어야 합니다.', type: ErrorResponseDto })
+    @ApiResponse({ status: 400, description: '회사명은 필수 입력 값입니다.', type: ErrorResponseDto })
     @ApiResponse({ status: 409, description: '이미 존재하는 회사 이름입니다.', type: ErrorResponseDto })
     @ApiResponse({ status: 500, description: '회사 생성과정 중 서버에서 에러가 발생했습니다.', type: ErrorResponseDto })
     async createCompany(@Body() createCompanyDto: CreateCompanyDto) {
