0bc53eeafa5a3c2bad3673a6ff15e534d526584b
Yoo-SH <ads137485@jbnu.ac.kr>
2024-12-15 17:40:31 +0900

    fix: ecosystem.config js 프로젝트 경로 수정

--- server/nest-server/ecosystem.config.js
+++ server/nest-server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'nest-server',
             script: 'dist/main.js',
-            cwd: '/server/nest-server', // 프로젝트 루트 경로
+            cwd: 'ls /home/ubuntu/saramin_server/server/nest-server/', // 프로젝트 루트 경로
             env_production: {
                 NODE_ENV: 'production',
             },
